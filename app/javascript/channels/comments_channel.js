7d691ec33298639892eaa778708f6d76fd7b2979
Frederico Dal Grande <dalgrande@gmail.com>
2020-10-22 15:35:48 -0300

    added comments and comments channel

---
+++ app/javascript/channels/comments_channel.js
@@ -0,0 +1,20 @@
+import consumer from "./consumer"
+
+consumer.subscriptions.create("CommentsChannel", {
+  connected() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    // Called when there's incoming data on the websocket for this channel
+    // function myFunction() {
+      document.getElementById("comments").innerHTML += (data.comment);
+    // }
+    // myFunction()
+    console.log(data)
+  }
+});
